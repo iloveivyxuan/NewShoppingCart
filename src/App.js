3f3f97d2ed8dea84338346d04ed2470a2648ba0b
ivyxuan <iloveivyxuan@gmail.com>
2020-01-21 00:32:59 -0600

    Fetch inventory from firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Shelf from './components/Shelf';
 import FloatCart from './components/FloatCart';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXsGHQ2R0A-2H2cY7EbNQEZA7FB28NpU4",
+  authDomain: "newshoppingcart-eccbc.firebaseapp.com",
+  databaseURL: "https://newshoppingcart-eccbc.firebaseio.com",
+  projectId: "newshoppingcart-eccbc",
+  storageBucket: "newshoppingcart-eccbc.appspot.com",
+  messagingSenderId: "699468622066",
+  appId: "1:699468622066:web:947e45a20974766e71886b",
+  measurementId: "G-EGZR7T90VR"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
 
 const App = () => {
   const [showCart, setShowCart] = useState(false);
@@ -8,12 +24,10 @@ const App = () => {
   const [inventory, setInventory] = useState({});
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch('./data/inventory.json');
-      const json = await response.json();
-      setInventory(json);
-    };
-    fetchProducts();
+    const updateInventory = (snapshot) => {
+      setInventory(snapshot.val());
+    }
+    db.once("value", updateInventory, error => alert(error));
   }, []);
 
   return (
